66c27299d7db89d763401d2748b8b21e3c6999f0
pawelwasik2 <pawelwasik18@gmail.com>
2019-12-18 18:02:09 +0100

    end of chapter 6

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,37 @@
 object Main extends App{
+  implicit def intToRational(x: Int) = new Rational(x)
+  //implicit conversion, more about to give it in scope in Chapter 21
+  //this implicit is need to make
+  //2 * r
+  //working, now its not, it works in REPL idk why (prob its not in scope), more in chapter 21
+
+
+  val x = new Rational(1, 2)
+  val y = new Rational(2, 3)
+
+  x + y
+  x + y * x
+
+  x * 2
 
 }
+
+/**Immutable objects offer several advantages over mutable objects, and one potential disadvantage. First,
+immutable objects are often easier to reason about than mutable ones, because they do not have
+complex state spaces that change over time. Second, you can pass immutable objects around quite
+freely, whereas you may need to make defensive copies of mutable objects before passing them to other
+code. Third, there is no way for two threads concurrently accessing an immutable to corrupt its state
+once it has been properly constructed, because no thread can change the state of an immutable. Fourth,
+immutable objects make safe hash table keys. If a mutable object is mutated after it is placed into
+a HashSet, for example, that object may not be found the next time you look into the HashSet.
+
+  //scala classes can takes parameters directly
+
+  //we shouldnt use $ in identifier's names even if program will compile
+  //name for constants values is camel-case with first letter upper like: XOffset, CCase
+
+  //camel-case = sthNameSth
+
+  //val even is created it's still a variable, constant is more permanent
+
+  // use `..` for reserved scala's words
--- src/main/scala/Rational.scala
+++ src/main/scala/Rational.scala
@@ -1,3 +1,50 @@
 class Rational (n: Int, d: Int){
 
+  require(d != 0)
+
+  override def toString = numer + "/" + denom //this override method from java.lang.Object to see proper value in fe. REPL instead of hashes and bushes
+
+  private val g = gcd(n.abs, d.abs)
+  val numer: Int = n / g
+  val denom: Int = d / g
+
+  def this(n: Int) = this(n, 1) // auxiliary constructor
+  //In Scala, every auxiliary constructor must invoke another constructor of the same class as its first action.
+  // In other words, the first statement in every auxiliary constructor in every Scala class will have the form "this(...)"
+  //Every auxiliary constructor invokes other constructors and it must ends up in promary constructor.
+
+  //The primary constructor is thus the single point of entry of a class.
+
+  def + (that: Rational): Rational =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+
+  def + (i: Int): Rational =
+    new Rational(numer + i * denom, denom)
+
+  def - (that: Rational): Rational =
+    new Rational(
+      numer * that.denom - that.numer * denom,
+      denom * that.denom
+    )
+
+  def - (i: Int): Rational =
+    new Rational(numer - i * denom, denom)
+
+  def * (that: Rational): Rational =
+    new Rational(numer * that.numer, denom * that.denom)
+
+  def * (i: Int): Rational =
+    new Rational(numer * i, denom)
+
+  def / (that: Rational): Rational =
+    new Rational(numer * that.denom, denom * that.numer)
+
+  def / (i: Int): Rational =
+    new Rational(numer, denom * i)
+
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
 }
